9207a830cea358000d33339d9883bf1ec084f751
Muhammad Abdullah <muhammada242@nycstudents.net>
2026-01-12 08:34:55 -0500

    done

--- script.js
+++ script.js
@@ -10,26 +10,41 @@ let brown_position = 1;
 // Query Selectors Here
 const blueButton = document.querySelector("#blue-button");
 const blueHorse = document.querySelector("#blue-horse");
-
+const pinkButton = document.querySelector("#pink-button");
+const pinkHorse = document.querySelector("#pink-horse");
+const brownButton = document.querySelector("#brown-button");
+const brownHorse = document.querySelector("#brown-horse");
+const winnerDiv = document.querySelector("#winner");
 
 
 // OnClick Functions Here
 const advanceBlue = (e) => {
   blue_position += 1;
   changePosition(blueHorse, blue_position);
+  checkWinner(blue_position, "blue");
 };
 const advancePink = (e) => {
-
+  pink_position += 1;
+  changePosition(pinkHorse, pink_position);
+  checkWinner(pink_position, "pink");
 };
-
+const advanceBrown = (e) => {
+  brown_position += 1;
+  changePosition(brownHorse, brown_position);
+  checkWinner(brown_position, "brown");
+};
 
 
 // Check for a winner
 const checkWinner = (position, color) => {
-
+  if (position > 5) {
+    winnerDiv.textContent = `${color.charAt(0).toUpperCase() + color.slice(1)} horse wins!`;
+  }
 };
 
 
 
 // Event Listeners Here
 blueButton.addEventListener("click", advanceBlue);
+pinkButton.addEventListener("click", advancePink);
+brownButton.addEventListener("click", advanceBrown);
